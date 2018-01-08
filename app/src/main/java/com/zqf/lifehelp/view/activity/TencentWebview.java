4bdffc70dcd38ae36141efd21088df3d8b3e0382
zhangqingf <1583510917@qq.com>
2018-01-08 17:24:15 +0800

    修改增加文件

---
+++ app/src/main/java/com/zqf/lifehelp/view/activity/TencentWebview.java
@@ -0,0 +1,10 @@
+package com.zqf.lifehelp.view.activity;
+
+/**
+ * class from
+ * Created by zqf
+ * Time 2018/1/8 15:56
+ */
+
+public class TencentWebview {
+}
---
+++ app/src/main/java/com/zqf/lifehelp/view/widget/WebViewJavaScriptFunction.java
@@ -0,0 +1,6 @@
+package com.zqf.lifehelp.view.widget;
+
+public interface WebViewJavaScriptFunction {
+
+	void onJsFunctionCalled(String tag);
+}
