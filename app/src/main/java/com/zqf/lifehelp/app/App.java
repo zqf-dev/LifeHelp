1a8aa2391fc58a41e59d942f14f5fb8e39cea6a6
eternityzqf <1583510917@qq.com>
2018-04-11 14:09:46 +0800

    add hxLibs and modified app.java

--- app/src/main/java/com/zqf/lifehelp/app/App.java
+++ app/src/main/java/com/zqf/lifehelp/app/App.java
@@ -1,10 +1,15 @@
 package com.zqf.lifehelp.app;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import com.blankj.utilcode.util.SPUtils;
 import com.blankj.utilcode.util.Utils;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.FormatStrategy;
 import com.orhanobut.logger.Logger;
@@ -14,6 +19,9 @@ import com.zqf.lifehelp.BuildConfig;
 import com.zqf.lifehelp.api.RetrofitHelper;
 import com.zqf.lifehelp.db.help.RealmHelp;
 import com.zqf.lifehelp.utils.LogUtil;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by zqf on 2017/5/31.
@@ -56,6 +64,54 @@ public class App extends Application {
         RetrofitHelper.getInstance().init();
         //初始化Realm数据库
         RealmHelp.getInstance().init(this);
+        //环信初始化
+        EMConnect();
+    }
+
+    private void EMConnect() {
+        EMOptions options = new EMOptions();
+        // 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+        // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
+        options.setAutoTransferMessageAttachments(true);
+        // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
+        options.setAutoDownloadThumbnail(true);
+
+        int pid = android.os.Process.myPid();
+        String processAppName = getAppName(pid);
+        // 如果APP启用了远程的service，此application:onCreate会被调用2次
+        // 为了防止环信SDK被初始化2次，加此判断会保证SDK被初始化1次
+        // 默认的APP会在以包名为默认的process name下运行，如果查到的process name不是APP的process name就立即返回
+        if (processAppName == null || !processAppName.equalsIgnoreCase(getPackageName())) {
+            Log.e("Tag", "enter the service process!");
+            // 则此application::onCreate 是被service 调用的，直接返回
+            return;
+        }
+        //初始化
+        EMClient.getInstance().init(this, options);
+        //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
+
+    }
+
+    private String getAppName(int pID) {
+        String processName = null;
+        ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        List l = am.getRunningAppProcesses();
+        Iterator i = l.iterator();
+        PackageManager pm = this.getPackageManager();
+        while (i.hasNext()) {
+            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo) (i.next());
+            try {
+                if (info.pid == pID) {
+                    processName = info.processName;
+                    return processName;
+                }
+            } catch (Exception e) {
+                Log.d("Process", "Error>> :" + e.toString());
+            }
+        }
+        return processName;
     }
 
     /**
